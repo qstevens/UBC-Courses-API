a2b4336a0480a3ebd973d3e8aad1dcc696fd3755
Stevens Qiu <stevensqiu@gmail.com>
2019-02-20 03:48:27 -0800

    testing out basic REST functionality

--- src/routes/person.js
+++ src/routes/person.js
@@ -5,20 +5,25 @@ let router = express.Router()
 // localhost:3000/person?name=stevens&age=21
 router.get('/person', (req, res) => {
     if(req.query.name) {
-        res.send(`You have requested a person ${req.query.name}`)
+        res.send(`You have requested a person ${req.query.name}\n`)
     } else {
-        res.send('You have requested a person')
+        res.send('You have requested a person\n')
     }
 })
 
 // Params property on the request object
 // localhost:3000/person/stevens
 router.get('/person/:name', (req, res) => {
-    res.send(`You have requested a person ${req.params.name}`)
+    res.send(`You have requested a person ${req.params.name}\n`)
 })
 
 router.get('/error', (req, res) => {
     throw new Error('This is a forced Error.')
+})
+
+router.get('/json', (req, res) => {
+    let jsonObject = {code: "CPSC"};
+    res.send(jsonObject);
 })
 
 module.exports = router
