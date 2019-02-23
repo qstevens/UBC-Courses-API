daba25b815fec53733802129b44a610533daba18
Stevens Qiu <stevensqiu@gmail.com>
2019-02-23 02:08:05 -0800

    changed start to node

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
+const ip = process.env.IP || '0.0.0.0';
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, ip);
