4726ce6c5b098dde681e5eb29dd9c3da8967c5e9
Stevens Qiu <stevensqiu@gmail.com>
2019-02-23 23:35:51 +0000

    simple home page

--- src/connection.js
+++ src/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = 
-   'mongodb+srv://read-user:' + 
-    process.env.MONGO_ATLAS_PW +
-    '@ubc-web-api-zvc60.mongodb.net/2018W?retryWrites=true';
 const sessionList = ['2018W', '2019S'];
+
 const connectionMap = {};
 
 for (let session of sessionList) {
