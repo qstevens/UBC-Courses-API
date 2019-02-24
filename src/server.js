9efb045e7dc6f7a0119c131da602161b159aca13
Stevens Qiu <stevensqiu@gmail.com>
2019-02-24 14:24:51 -0800

    added API reference

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
-const ip = process.env.IP || '0.0.0.0';
+// const ip = process.env.IP || '0.0.0.0';
 
 const server = http.createServer(app.app);
 
-server.listen(port, ip);
+server.listen(port);
