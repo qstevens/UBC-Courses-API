0fa1e70fd33a9f73d25b9cd75d37ab6427f4038d
Stevens Qiu <stevensqiu@gmail.com>
2019-06-02 12:32:34 -0700

    added json spaces 4

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const connectionMap = require('./connection');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.set('json spaces', 4);
 
 // appends response header
 app.use((req, res, next) => {
