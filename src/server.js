29b13b60834c656fdbda0fed9efc005255c4ff38
stevens <stevensqiu@gmail.com>
2020-03-11 21:05:19 -0700

    add 2020S

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
-// const ip = process.env.IP || '0.0.0.0';
 
 const server = http.createServer(app);
 
